a01c52021c75e9bbe83bc71c71331ed86c4cca5e
Joshua Li <106456846+JoshL589@users.noreply.github.com>
2022-10-06 17:49:25 +1000

    Time: 193 ms (10.24%), Space: 14.2 MB (52.82%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        letters = {}
+        
+        for i in magazine:
+            letters[i] = letters.get(i, 0) + 1
+        
+        for i in ransomNote:
+            if i not in letters or letters[i] <= 0:
+                return False
+            else:
+                letters[i] -= 1
+        
+        return True
