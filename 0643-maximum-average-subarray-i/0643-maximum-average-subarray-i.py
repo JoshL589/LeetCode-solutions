afea08f3c2c7a078bc2d2fa1ad08d675bc979afe
Joshua Li <106456846+JoshL589@users.noreply.github.com>
2023-01-09 13:30:36 +1000

    Time: 1507 ms (61.36%), Space: 26.2 MB (40.61%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,11 +3,11 @@ class Solution:
         l = 0
         r = k - 1
         curr = 0
-        ans = -1000000000
+        
         for i in range(k):
             curr += nums[i]
             
-        ans = max(ans, curr/k)
+        ans = curr/k
         
         for i in range(k, len(nums)):
             curr -= nums[i -k]
