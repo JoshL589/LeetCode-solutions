ce67e18bcb8db3a8f72a0733a13eecb5e5210ff9
Joshua Li <106456846+JoshL589@users.noreply.github.com>
2023-01-09 12:54:19 +1000

    Time: 54 ms (62.00%), Space: 14.3 MB (21.21%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        #map a unique letter to the index, unique letters can be represented by equal numbers
+        
+        dic1 = {}
+        dic2 = {}
+        for i in range(len(s)):
+            if s[i] in dic1:
+                if dic1[s[i]] != t[i]:
+                    return False
+            if t[i] in dic2:
+                if dic2[t[i]] != s[i]:
+                    return False
+            dic1[s[i]] = t[i]
+            dic2[t[i]] = s[i]
+
+        return True
