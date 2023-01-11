8018b3847f2c2bff92c63f9d34dc97a37cfbd8c6
Joshua Li <106456846+JoshL589@users.noreply.github.com>
2023-01-11 18:11:18 +1000

    Time: 39 ms (99.08%), Space: 14 MB (83.87%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -3,19 +3,15 @@ class Solution:
         dic1 = {}
         dic2 = {}
         
-        for i in nums1:
-            dic1[i] = dic1.get(i, 0) + 1
-        
         for i in nums2:
             dic2[i] = dic2.get(i, 0) + 1
         
         ans = []
         
         for i in nums1:
-            if i in dic2 and dic2[i] > 0 and i in dic1 and dic1[i] > 0:
+            if i in dic2 and dic2[i] > 0:
                 ans.append(i)
                 dic2[i] -= 1
-                dic1[i] -= 1
                 
         
         return ans
