46524d35a987bb19623f62f148d998f4a7ee9300
Joshua Li <106456846+JoshL589@users.noreply.github.com>
2023-01-11 18:15:13 +1000

    Time: 1121 ms (75.80%), Space: 25.1 MB (37.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #profit is buying for cheap and selling it a different day, l is buying r is selling, you should only update l if you can buy a cheaper one and you keep checking how much profit you can make by checking the difference between l and r
         l = 0
-        r = 0
-        max_profit = 0
-        while r < len(prices):
-            print(max_profit)
-            max_profit = max(max_profit, prices[r] - prices[l])
+        r = 1
+        ans = 0
+        
+        while r <len(prices):
             if prices[r] < prices[l]:
                 l = r
+            cur = prices[r] - prices[l]
             r += 1
-        
-        return max_profit
+            ans = max(ans, cur)
+            
+        return ans
